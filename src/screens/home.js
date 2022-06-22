e8071e0199a5f0c85c8565560fe950baa8da7662
RHsanto <rakibulsanto789@gmail.com>
2022-06-23 01:33:46 +0600

    add details page

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,4 +1,4 @@
-import { View, SafeAreaView, StyleSheet, FlatList} from "react-native";
+import { View, SafeAreaView, StyleSheet, FlatList, Pressable} from "react-native";
 import React from "react";
 import Text from "../components/text/text";
 import { colors } from "../theme/colors";
@@ -7,7 +7,7 @@ import { PLANET_LIST } from "../data/planet.data";
 import { spacing } from "../theme/spacing";
 import { AntDesign } from '@expo/vector-icons';
 
-export default function Home(){
+export default function Home({navigation}){
   return (
    <SafeAreaView style={styles.container}>
    <PlanetHeader/> 
@@ -19,13 +19,17 @@ export default function Home(){
    renderItem={({item,index})=>{
     const {name, color}= item
       return (
-        <View style={styles.item}>
+        <Pressable  onPress={()=>{
+         navigation.navigate('Details')
+        }}
+        style={styles.item}>
           <View style={{flexDirection:'row', alignItems:'center'}}>
           <View style={[styles.circle, {backgroundColor:color}]} />
           <Text preset="h4" style={styles.itemName}>{name}</Text>
           </View>
+
           <AntDesign name="right" size={24} color="white" />
-        </View>
+        </Pressable>
       )
    }}
    ItemSeparatorComponent={()=> <View style={styles.separator}/>}
@@ -42,17 +46,18 @@ const styles = StyleSheet.create({
   item:{
     flexDirection:'row',
     alignItems:'center',
-    justifyContent:'space-between'
+    justifyContent:'space-between',
+    padding:spacing[4]
   },
   itemName:{
     textTransform:"uppercase",
-    marginLeft:3
+    marginLeft:spacing[5]
   },
   circle:{
     width:30,
     height: 30,
     borderRadius:15,
-    margin:spacing[4]
+   
   },
   lists:{
     padding:spacing[7]
