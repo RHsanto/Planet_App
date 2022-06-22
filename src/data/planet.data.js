bce4587f251a47042ebe5673c10d9e66f70874e3
RHsanto <rakibulsanto789@gmail.com>
2022-06-22 15:58:39 +0600

    [add] planet data

---
+++ src/data/planet.data.js
@@ -0,0 +1,63 @@
+export const PLANET_LIST = [
+
+  {
+   name: "mercury",
+   color: "#DEF4FC",
+   description:"Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.",
+   rotationTime: 58.6,
+   revolutionTime: "87.97 days",
+   radius: 2439.7,
+   avgTemp: "430°c",
+   wikiLink: "https://en.wikipedia.org/wiki/Mercury"
+  } ,
+  {
+   name: "mercury",
+   color: "#DEF4FC",
+   description:"Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.",
+   rotationTime: 58.6,
+   revolutionTime: "87.97 days",
+   radius: 2439.7,
+   avgTemp: "430°c",
+   wikiLink: "https://en.wikipedia.org/wiki/Mercury"
+  } ,
+  {
+   name: "mercury",
+   color: "#DEF4FC",
+   description:"Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.",
+   rotationTime: 58.6,
+   revolutionTime: "87.97 days",
+   radius: 2439.7,
+   avgTemp: "430°c",
+   wikiLink: "https://en.wikipedia.org/wiki/Mercury"
+  } ,
+  {
+   name: "mercury",
+   color: "#DEF4FC",
+   description:"Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.",
+   rotationTime: 58.6,
+   revolutionTime: "87.97 days",
+   radius: 2439.7,
+   avgTemp: "430°c",
+   wikiLink: "https://en.wikipedia.org/wiki/Mercury"
+  } ,
+  {
+   name: "mercury",
+   color: "#DEF4FC",
+   description:"Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.",
+   rotationTime: 58.6,
+   revolutionTime: "87.97 days",
+   radius: 2439.7,
+   avgTemp: "430°c",
+   wikiLink: "https://en.wikipedia.org/wiki/Mercury"
+  } ,
+  {
+   name: "mercury",
+   color: "#DEF4FC",
+   description:"Mercury is the smallest planet in the Solar System and the closest to the Sun. Its orbit around the Sun takes 87.97 Earth days, the shortest of all the Sun's planets. Mercury is one of four terrestrial planets in the Solar System, and is a rocky body like Earth.",
+   rotationTime: 58.6,
+   revolutionTime: "87.97 days",
+   radius: 2439.7,
+   avgTemp: "430°c",
+   wikiLink: "https://en.wikipedia.org/wiki/Mercury"
+  } ,
+]
