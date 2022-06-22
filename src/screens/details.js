032407c61ad9fbcc2564207ffbaec0559e7f362f
RHsanto <rakibulsanto789@gmail.com>
2022-06-23 03:31:39 +0600

    [done] details page

--- src/screens/details.js
+++ src/screens/details.js
@@ -1,14 +1,22 @@
-import { SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
+import { Pressable, SafeAreaView, ScrollView, StyleSheet, View } from "react-native";
 import Text from "../components/text/text";
 import React from 'react';
 import PlanetHeader from "../components/text/planet.header";
 import { colors } from "../theme/colors";
 import { spacing } from "../theme/spacing";
 
+const PlanetSection =({title,value})=>{
+  return(
+    <View style={styles.planetSection}>
+      <Text preset="small" style={{textTransform:'uppercase'}}>{title}</Text>
+      <Text preset="h3">{value}</Text>
+    </View>
+  )
+}
 
 export default function Details({navigation, route}){
   const planet = route.params.planet;
-  const {name,description}= planet;
+  const {name,description,rotationTime,revolutionTime,radius,avgTemp}= planet;
 
   return(
    <SafeAreaView style={styles.container}>
@@ -17,7 +25,16 @@ export default function Details({navigation, route}){
     <View style={styles.detailsView}>
       <Text preset="h1" style={styles.detailsTitle}>{name}</Text>
       <Text style={styles.detailsDesc}>{description}</Text>
+      <Pressable style={styles.source}>
+        <Text>Source:</Text>
+        <Text preset="h4" style={styles.wikipedia}>Wikipedia</Text>
+      </Pressable>
+
     </View>
+      <PlanetSection title='rotationTime' value={rotationTime} />
+      <PlanetSection title='revolutionTime' value={revolutionTime} />
+      <PlanetSection title='radius' value={radius} />
+      <PlanetSection title='avgTemp' value={avgTemp} />
     </ScrollView>
    </SafeAreaView>
   )
@@ -40,6 +57,29 @@ const styles =StyleSheet.create({
   detailsDesc:{
     textAlign:'center',
     marginTop:spacing[3],
-    lineHeight:26
+    lineHeight:28,
+    
+  },
+  source:{
+    flexDirection:'row',
+    marginTop:spacing[8],
+    alignItems:'center'
+  },
+  wikipedia:{
+    textDecorationLine:'underline',
+    fontWeight:'bold',
+    marginTop:5,
+    marginLeft:5
+  },
+  planetSection:{
+    flexDirection:'row',
+    alignItems:'baseline',
+    justifyContent:'space-between',
+    paddingHorizontal:spacing[5],
+    paddingVertical:spacing[4],
+    borderWidth:1,
+    borderColor:colors.grey,
+    marginHorizontal:spacing[6],
+    marginBottom:spacing[4]
   }
 })
